c9411100610860c2db5ee8effe6963427d1935d0
MananDesai54 <manan5401desai@gmail.com>
2020-12-30 23:44:14 +0530

    readme update

--- server/src/models/relative.js
+++ server/src/models/relative.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 
 const relativeSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String },
-    mobileNumber: { type: Number, required: true },
-    email: { type: String, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    mobileNumber: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
   },
   {
     timestamps: true,
