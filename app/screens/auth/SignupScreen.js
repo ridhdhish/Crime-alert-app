3e98db84d6d283662edb9aeb2002a7c1f5100109
MananDesai54 <manan5401desai@gmail.com>
2021-01-08 15:52:25 +0530

    register and login forms

--- app/screens/auth/SignupScreen.js
+++ app/screens/auth/SignupScreen.js
@@ -1,12 +1,52 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+import { colors } from "../../colors";
+import LogoText from "../../components/LogoText";
+import { Formik } from "formik";
 
 const SignupScreen = (props) => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Sign Up</Text>
+    <View style={styles.screen}>
+      <LogoText title="Register" />
+      <Formik
+        initialValues={{
+          firstname: "",
+          lastname: "",
+          password: "",
+          email: "",
+          DOB: "",
+          mobileNumber: "",
+          address: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, handleChange, handleSubmit }) => (
+          <TextInput
+            value={values.firstname}
+            onChangeText={handleChange("firstname")}
+            placeholder="Firstname"
+            style={styles.input}
+          />
+        )}
+      </Formik>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 16,
+  },
+  input: {
+    borderBottomColor: colors.textAccent,
+    borderBottomWidth: 1,
+    padding: 2,
+    width: "70%",
+  },
+});
 
 export default SignupScreen;
