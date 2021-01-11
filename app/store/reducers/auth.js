55f6afb91c4322bf7c2c34fa2de6ab29100b8ef0
MananDesai54 <manan5401desai@gmail.com>
2021-01-12 01:55:49 +0530

    configured redux

---
+++ app/store/reducers/auth.js
@@ -0,0 +1,11 @@
+const initState = {
+  user: null,
+  isLoading: false,
+  error: null,
+  token: null,
+  tokenExpiration: null,
+};
+
+export const authReducer = (state = initState, action) => {
+  return state;
+};
