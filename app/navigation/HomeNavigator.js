f2d318c0dc66262aa8ba64ab9dce145dbe7cff57
MananDesai54 <manan5401desai@gmail.com>
2021-01-18 12:48:58 +0530

    added more navigation

--- app/navigation/HomeNavigator.js
+++ app/navigation/HomeNavigator.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import { colors } from "../colors";
+import PlacesScreen from "../screens/PlacesScreen";
+import RelativesScreen from "../screens/RelativesScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 
 const HomeNavigator = () => {
+  const HomeTabs = createMaterialBottomTabNavigator();
   const HomeStack = createStackNavigator();
 
   return (
-    <HomeStack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: colors.backgroundPrimary },
-        headerTintColor: colors.textSecondary,
-      }}
-    >
+    <HomeTabs.Navigator screenOptions={{}} shifting={false}>
       <HomeStack.Screen
         name="Home"
         component={HomeScreen}
         options={{ title: "Home" }}
       />
-    </HomeStack.Navigator>
+      <HomeStack.Screen
+        name="Places"
+        component={PlacesScreen}
+        options={{ title: "Places" }}
+      />
+      <HomeStack.Screen
+        name="Relatives"
+        component={RelativesScreen}
+        options={{ title: "Relatives" }}
+      />
+      <HomeStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ title: "Profile" }}
+      />
+    </HomeTabs.Navigator>
   );
 };
 
---
+++ app/screens/RelativesScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RelativesScreen = () => {
+  return (
+    <View>
+      <Text>Relatives</Text>
+    </View>
+  );
+};
+
+export default RelativesScreen;
