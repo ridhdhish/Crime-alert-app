fab0c34d8e176556291aed232c1be98d04849450
MananDesai54 <manan5401desai@gmail.com>
2021-01-23 18:55:26 +0530

    alert for not authenticated users

--- src/controllers/crime.js
+++ src/controllers/crime.js
@@ -5,54 +5,39 @@ const Relative = require("../models/relative");
 const sendResponse = require("../utils/sendResponse");
 const jwt = require("jsonwebtoken");
 const { sendMail } = require("../utils/sendMail");
+const keyValueDb = require("../models/keyValueDb");
+const { SECRET_TOKEN_PREFIX } = require("../config/constant");
 
 const registerCrime = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return sendResponse(errors.array(), res, 400);
   }
-
+  let userId;
   const authHeader = req.get("Authorization");
   try {
-    //user is not authenticated but need help
-    // need to get one token to report spam
+    /**
+     * user is not authenticated but need help
+     * need to get one token to report spam
+     */
     if (!authHeader) {
-      const { firstName, lastName, mobileNumber, address } = req.body;
-      const { location, city, state, address: area } = req.body;
-
-      const place = new Place({
-        location,
-        city,
-        state,
-        address,
+      const { secretToken } = req.body;
+      if (!secretToken) {
+        return sendResponse("Don't try to make fake alerts", res, 401);
+      }
+      const user = await keyValueDb.findOne({
+        key: `${SECRET_TOKEN_PREFIX}${secretToken}`,
       });
-      await place.save();
-
-      const crime = new Crime({
-        userData: {
-          firstName: firstName || "Unknown",
-          lastName: lastName || "Unknown",
-          mobileNumber: mobileNumber || "Unknown",
-          address: area || "Unknown",
-        },
-        placeId: place.id,
-      });
-      await crime.save();
+      if (!user) {
+        return sendResponse("Don't try to make fake alerts", res, 401);
+      }
+      userId = user.value.id;
 
       /**
-       * @Todo
-       * Send crime data to police
+       * user is authenticated and need help
        */
-
-      return sendResponse("Crime reported successfully", res, 200);
-
-      //user is authenticated and need help
     } else {
       const token = authHeader.split(" ")[1];
-
-      /**
-       * @Doubt
-       */
       if (!token) {
         return sendResponse("Don't try to make fake alerts", res, 401);
       }
@@ -62,44 +47,45 @@ const registerCrime = async (req, res) => {
           message: "Don't try to make fake alerts",
         });
       }
-      const { location, city, state, address } = req.body;
-      const place = new Place({
-        location,
-        city,
-        state,
-        address,
-      });
-      await place.save();
-
-      const crime = new Crime({
-        userId: decodedToken.user.id,
-        placeId: place.id,
-      });
-
-      await crime.save();
-
-      /**
-       * @Todo
-       * Send notification to relative, bluetooth near by
-       * Send crime to police
-       * send message, mail, call to relative
-       */
-
-      const relatives = await Relative.find({ userId: decodedToken.user.id });
-
-      relatives.forEach(async (rel) => {
-        await sendMail(rel.email, `Mail sent`);
-      });
-      sendResponse(
-        {
-          message: "Crime reported successfully",
-          crime,
-          place,
-        },
-        res,
-        200
-      );
+      userId = decodedToken.user.id;
     }
+    const { location, city, state, address } = req.body;
+    const place = new Place({
+      location,
+      city,
+      state,
+      address,
+    });
+    await place.save();
+
+    const crime = new Crime({
+      userId,
+      placeId: place.id,
+    });
+
+    await crime.save();
+
+    /**
+     * @Todo
+     * Send notification to relative, bluetooth near by
+     * Send crime to police
+     * send message, mail, call to relative
+     */
+
+    const relatives = await Relative.find({ userId: decodedToken.user.id });
+
+    relatives.forEach(async (rel) => {
+      await sendMail(rel.email, `Mail sent`);
+    });
+    sendResponse(
+      {
+        message: "Crime reported successfully",
+        crime,
+        place,
+      },
+      res,
+      200
+    );
   } catch (error) {
     sendResponse(error.message, res);
   }
