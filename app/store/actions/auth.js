91ae2a80b91e2dbf1bb132e46ff0be5147de1354
MananDesai54 <manan5401desai@gmail.com>
2021-01-24 15:20:46 +0530

    storing push tokens

--- app/store/actions/auth.js
+++ app/store/actions/auth.js
@@ -22,12 +22,13 @@ export const signup = ({
     mobileNumber,
     address,
   };
+  const pushToken = await JSON.parse(AsyncStorage.getItem("pushToken"));
   const response = await fetch(`${env.API_URL}/auth/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(authData),
+    body: JSON.stringify({ ...authData, pushToken }),
   });
   const body = await response.json();
   const expirationTime = new Date(
--- src/models/user.js
+++ src/models/user.js
@@ -26,6 +26,9 @@ const UserSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    pushToken: {
+      type: String,
+    },
   },
   {
     timestamps: true,
