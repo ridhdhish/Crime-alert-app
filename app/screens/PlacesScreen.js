deae3be140d25d0cefa418dd3cb8cf255d6bec9c
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-02-01 14:10:02 +0530

    chart

--- app/screens/PlacesScreen.js
+++ app/screens/PlacesScreen.js
@@ -1,10 +1,55 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
+
+import { LineChart, BarChart } from "react-native-chart-kit";
 
 const PlacesScreen = () => {
   return (
     <View>
       <Text>Places</Text>
+      <BarChart
+        data={{
+          labels: ["January", "February", "March", "April", "May"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={300}
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1}
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          marginLeft: 10,
+          borderRadius: 16,
+        }}
+      />
     </View>
   );
 };
