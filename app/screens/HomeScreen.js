677c7d37e539325e3e572a7ca57084a5f6a875a5
MananDesai54 <manan5401desai@gmail.com>
2021-02-03 22:44:56 +0530

    custom kms for around area

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -12,7 +12,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { colors } from "../colors";
 import FloatingButton from "../components/FloatingButton";
 import { getAroundData, reportCrime } from "../store/actions/crime";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { getCrimeData } from "../utils/getCrimeData";
 import { sendNotification } from "../utils/sendNotification";
 import AlertButton from "../components/AlertButton";
@@ -95,7 +95,7 @@ const HomeScreen = (props) => {
         <Fragment>
           <FloatingButton
             style={{
-              left: 20,
+              left: 15,
               top: 25,
             }}
             onPress={() => props.navigation.toggleDrawer()}
@@ -165,6 +165,28 @@ const HomeScreen = (props) => {
               </View>
             )}
           </FloatingButton>
+          {JSON.stringify(markerPosition) !==
+            JSON.stringify(currentLocation) && (
+            <FloatingButton
+              style={{
+                bottom: 30,
+                left: 15,
+                width: 100,
+                flexDirection: "row",
+                elevation: 5,
+                borderColor: colors.textSecondary,
+                borderWidth: 1,
+              }}
+              onPress={() => setMarkerPosition(currentLocation)}
+            >
+              <MaterialIcons
+                name="navigation"
+                size={20}
+                color={colors.textSecondary}
+              />
+              <Text style={{ color: colors.textSecondary }}>Recenter</Text>
+            </FloatingButton>
+          )}
           {addCrimeData && (
             <View
               style={{
@@ -226,7 +248,7 @@ const HomeScreen = (props) => {
               </Marker>
               <Circle
                 center={markerPosition}
-                radius={3300}
+                radius={3000}
                 fillColor={getCriticalColor(
                   crimePlaces.places.length,
                   crimePlaces.totalCrimes
