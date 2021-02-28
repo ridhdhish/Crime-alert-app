3195dc2bedf8a21303b35ca3d6bfcfcda6b2777c
MananDesai54 <manan5401desai@gmail.com>
2021-02-28 11:51:57 +0530

    bug resolve

--- src/controllers/crime.js
+++ src/controllers/crime.js
@@ -210,7 +210,7 @@ const registerCrime = async (req, res) => {
 const seenCrime = async (req, res) => {
   const { crimeId } = req.params;
   try {
-    const userData = await User(req.user.id);
+    const userData = await User.findById(req.user.id);
     const alert = await userData.recentAlerts.find(
       (alert) => alert.crimeId.toString() === crimeId
     );
